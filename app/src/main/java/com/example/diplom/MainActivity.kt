8af274d7baa0ea900beb18f5ef07941c7aade81c
Vladislav <zv97@mail.ru>
2020-03-09 17:32:53 +0600

    set bitmap image in imageView

--- app/src/main/java/com/example/diplom/MainActivity.kt
+++ app/src/main/java/com/example/diplom/MainActivity.kt
@@ -5,22 +5,35 @@ import android.os.Bundle
 import android.provider.MediaStore
 import android.content.Intent
 import android.graphics.Bitmap
-import android.view.View
+import android.media.Image
 import android.widget.Button
+import android.widget.ImageView
 
 
 class MainActivity : AppCompatActivity() {
-
+    val REQUEST_IMAGE_CAPTURE = 1
+    private lateinit var photo: Button
+    private lateinit var imagePhoto: ImageView
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val camera = findViewById<Button>(R.id.camera)
-        camera.setOnClickListener{
+
+        photo = findViewById<Button>(R.id.photo)
+        imagePhoto = findViewById<ImageView>(R.id.imagePhoto)
+
+        photo.setOnClickListener{
             dispatchTakePictureIntent()
         }
+
     }
 
-    val REQUEST_IMAGE_CAPTURE = 1
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            imagePhoto.setImageBitmap(imageBitmap)
+        }
+    }
 
     private fun dispatchTakePictureIntent() {
         Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
@@ -29,4 +42,6 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
+
 }
