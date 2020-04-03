a67123a254d850ba03a2a2aaad3d54c670528b40
Vladislav <zv97@mail.ru>
2020-04-03 19:18:40 +0600

    create basic drawer

--- app/src/main/java/com/example/diploma/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/diploma/ui/main/MainActivity.kt
@@ -2,6 +2,11 @@ package com.example.diploma.ui.main
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.diploma.R
 import com.example.diploma.base.BaseActivity
@@ -9,20 +14,23 @@ import com.example.diploma.injection.component.DaggerMainActivityInjector
 import com.example.diploma.model.Record
 import com.example.diploma.ui.record.RecordActivity
 import com.example.diploma.utils.adapters.RecordsAdapter
-import kotlinx.android.synthetic.main.activity_main.*
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_all_records.*
+import kotlinx.android.synthetic.main.activity_main_drawer.*
 import kotlinx.android.synthetic.main.toolbar.*
 import java.util.*
 import javax.inject.Inject
 
-class MainActivity : BaseActivity<MainPresenter>(), MainView, RecordsAdapter.OnRecordListener {
+class MainActivity : BaseActivity<MainPresenter>(), MainView, RecordsAdapter.OnRecordListener,
+    NavigationView.OnNavigationItemSelectedListener {
 
     @Inject
     lateinit var recordsAdapter: RecordsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar_actionbar)
+        setContentView(R.layout.activity_main_drawer)
+        initDrawer()
         DaggerMainActivityInjector
             .builder()
             .listener(this)
@@ -42,6 +50,16 @@ class MainActivity : BaseActivity<MainPresenter>(), MainView, RecordsAdapter.OnR
     private fun initAdapter() {
         records.layoutManager = LinearLayoutManager(this)
         records.adapter = recordsAdapter
+    }
+
+    private fun initDrawer() {
+        setSupportActionBar(toolbar)
+        val toggle = ActionBarDrawerToggle(
+            this, drawer_layout, toolbar, R.string.app_name, 0
+        )
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+        nav_view.setNavigationItemSelectedListener(this)
     }
 
     override fun setRecords(records: Map<UUID, Record>) {
@@ -66,5 +84,27 @@ class MainActivity : BaseActivity<MainPresenter>(), MainView, RecordsAdapter.OnR
         val intent = Intent(this, RecordActivity::class.java)
         intent.putExtra(Record::class.java.simpleName, record)
         startActivity(intent)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_profile -> {
+                Toast.makeText(this, "Profile clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_messages -> {
+                Toast.makeText(this, "Messages clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_friends -> {
+                Toast.makeText(this, "Friends clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_update -> {
+                Toast.makeText(this, "Update clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_logout -> {
+                Toast.makeText(this, "Sign out clicked", Toast.LENGTH_SHORT).show()
+            }
+        }
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
     }
 }
